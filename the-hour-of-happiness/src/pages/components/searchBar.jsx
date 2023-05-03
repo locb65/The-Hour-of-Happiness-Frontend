1c90f6400940429745c2f9e963fcd2a44f83c982
Loc Bui <buda@Locs-MacBook-Pro.local>
2023-05-03 08:51:44 -0400

    added some basic file setup, made simple search bar

---
+++ the-hour-of-happiness/src/pages/components/searchBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SearchBar = () => {
+    return (
+        <div className="searchBar-container">
+            <input type="search" placeholder="Search">Search</input>
+        </div>
+    )
+}
