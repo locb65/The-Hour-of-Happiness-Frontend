d29e382293bdc530d3718a64fea8dcb1b25c1960
locb65 <locb65@gmail.com>
2023-05-06 17:40:16 -0400

    added state handlers and navigation to age verification.

--- the-hour-of-happiness/src/pages/accessDenied.jsx
+++ the-hour-of-happiness/src/pages/accessDenied.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export const AccessDenied = () => {
     return (
         <div className="access-denied-container">
             <h2>Access Denied: You must be 21 or older to drink</h2>
+            <Link to="/">Back to Home</Link>
         </div>
     )
 }
--- the-hour-of-happiness/src/pages/components/verifyAge.jsx
+++ the-hour-of-happiness/src/pages/components/verifyAge.jsx
@@ -2,17 +2,50 @@ import React from "react";
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import './verifyAge.css'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 // added age verification since this is based on alcholic beverages
 export const AgeVerificationForm = () => {
+    // state for DOB verification
+    const [dob, setDob] = useState(new Date());
+    const navigate = useNavigate();
+
+    const handleVerification = () => {
+        // calculates the age diff. 
+        // Date.now() gives current date
+        // dob.getTime() gives dob selected from the calendar
+        const ageDifference = Date.now() - dob.getTime();
+        // sets age from above to new date giving age of user
+        const ageDate = new Date(ageDifference);
+        // calculates the age of the user based on years 
+        // uses 1900 as a base point as I doubt anyone is alive still that is born before 1900
+        const age = Math.abs(ageDate.getUTCFullYear() -1900);
+        
+        if (age <21) {
+            navigate('/access-denied')
+        } else {
+            navigate('/login-choice')
+        }
+    }
+
     return (
         <div className="age-verification-container">
             <h2>Are You Above 21 Years Old?</h2>
             <p>Please select your Date of Birth to Conitnue</p>
             <div className="calendar-wrapper">
-                <Calendar className="custom-calendar" maxDate={new Date()}/>
+                <Calendar 
+                className="custom-calendar" 
+                value = {dob}
+                onChange = {setDob}
+                maxDate={new Date()}/>
             </div>
-            <button className="submit-dob" type="submit">Verify</button>
+            <button 
+            className="submit-dob" 
+            type="submit"
+            onClick={handleVerification}>
+                Verify
+            </button>
         </div>
     )
 }
