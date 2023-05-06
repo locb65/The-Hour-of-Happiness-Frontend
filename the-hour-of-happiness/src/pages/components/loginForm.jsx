a98f08a9ba3f525b8ae9e326262817627abcad27
locb65 <locb65@gmail.com>
2023-05-06 15:50:35 -0400

    applied css to login form and page, renamed login --> loginform

--- the-hour-of-happiness/src/pages/components/loginForm.jsx
+++ the-hour-of-happiness/src/pages/components/loginForm.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './loginForm.css'
 
 export const Login = () => {
     return (
-        <div className="login-container">
+        <div className="login-form-container">
             <form className="login-form">
                 <label className="login-fields">
                     Email:
--- the-hour-of-happiness/src/pages/loginPage.jsx
+++ the-hour-of-happiness/src/pages/loginPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Login } from './components/login';
+import { Login } from './components/loginForm';
+import './loginPage.css'
 
 export const LoginPage = () => {
     return (
-        <div>
+        <div className='login-container'>
             <h1>Welcome to Clink City!</h1>
             <h2>Please Login or Create an Account</h2>
             <Login/>
