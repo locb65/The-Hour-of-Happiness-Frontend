06ea59db3b02328270cdf39f82a0dc018b5b2767
locb65 <locb65@gmail.com>
2023-05-06 17:22:17 -0400

    setup App.js to use react-Router

--- the-hour-of-happiness/src/App.jsx
+++ the-hour-of-happiness/src/App.jsx
@@ -3,16 +3,19 @@ import { HomeView } from './pages/home';
 import { LoginChoice } from './pages/loginChoicePage';
 import { LoginPage } from './pages/loginPage';
 import { WelcomePage } from './pages/welcomePage';
+import { AccessDenied } from './pages/accessDenied';
+import { Route, Routes} from 'react-router-dom'
+
 
 export const App = () => {
   return (
-    <div className='App'>
-      {/* <HomeView/> */}
-      {/* <LoginChoice/> */}
-      {/* <LoginPage/> */}
-      <WelcomePage/>
-
-    </div>
+    <Routes>
+      <Route path='/' element={<WelcomePage/>} />
+      {/* <Route path="/home" element={<HomeView />} /> */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/login-choice" element={<LoginChoice />} />
+      <Route path="/access-denied" element={<AccessDenied />} />
+    </Routes>
   )
 }
 
