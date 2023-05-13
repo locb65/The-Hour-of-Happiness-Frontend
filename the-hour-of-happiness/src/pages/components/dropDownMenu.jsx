1816c8163a3688bd026bbbf25939658dac57fd16
locb65 <locb65@gmail.com>
2023-05-13 15:32:19 -0400

    created a dropdown menu template

---
+++ the-hour-of-happiness/src/pages/components/dropDownMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DropDownMenu.css';
+
+export const DropDownMenu = () => {
+    return (
+        <div className='dropdown-container'>
+            <button>
+                Menu
+            </button>
+        </div>
+    )
+}
--- the-hour-of-happiness/src/pages/profilePage.jsx
+++ the-hour-of-happiness/src/pages/profilePage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { ProfileHeader } from "./components/profileHeader";
+import { Header } from "./components/header";
 
 export const ProfilePage = () =>{
     return (
-        <div className="profile-page-container">
-            <ProfileHeader/>
+        <div>
+            <Header/>
+            <div className="profile-page-container"></div>
+                <ProfileHeader/>
         </div>
     )
 }
