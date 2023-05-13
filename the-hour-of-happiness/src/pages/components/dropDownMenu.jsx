3667354b223c8793c8bbe22a589987b8c87ed470
locb65 <locb65@gmail.com>
2023-05-13 15:40:03 -0400

    added links and state to toggle dropdown

--- the-hour-of-happiness/src/pages/components/dropDownMenu.jsx
+++ the-hour-of-happiness/src/pages/components/dropDownMenu.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
 import './DropDownMenu.css';
 
 export const DropDownMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    // handler for toggling dropdown
+    const toggleMenu = () =>{
+        // sets isOpen to the opposite of its current value
+        setIsOpen(!isOpen);
+    }
     return (
         <div className='dropdown-container'>
-            <button>
+            <button className='dropdown-btn' onClick={toggleMenu}>
                 Menu
             </button>
+            {isOpen && (
+                <ul className='dropdown-menu'>
+                    <Link to='/profile'><li>Profile</li></Link>
+                    <Link><li>Map</li></Link>
+                    <Link><li>Account</li></Link>
+                    <Link><li>Log out</li></Link>
+                </ul>
+            )}
         </div>
     )
 }
