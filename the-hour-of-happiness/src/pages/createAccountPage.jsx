6293c7406e8b6fd91c25d4116a69ede56a0c694d
locb65 <locb65@gmail.com>
2023-05-13 16:30:01 -0400

    created template for CreateAccountPage

---
+++ the-hour-of-happiness/src/pages/createAccountPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { CreateAccount } from "./components/createAccount";
+
+export const CreateAccountPage = () => {
+    return (
+        <div>
+            <h1>
+                Please fill in the Fields Below.
+            </h1>
+            <CreateAccount/>
+        </div>
+    )
+}
