1a33bad6188af16cfd9e01887160055be2e2822a
locb65 <locb65@gmail.com>
2023-05-21 19:31:03 -0400

    commented out last changes due to breaking application

--- the-hour-of-happiness/src/pages/components/profileHeader.jsx
+++ the-hour-of-happiness/src/pages/components/profileHeader.jsx
@@ -1,52 +1,62 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import { captureRejectionSymbol } from 'events';
 
 export const ProfileHeader = ({ sessionUser }) => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [newName, setNewName] = useState(sessionUser?.name)
-
-    const handleEditClick = () => {
-        setIsEditing(true);
-    }
-
-    const HandleNameChange = (e) => {
-        setNewName(e.target.value);
-    }
-
-    const handleSaveClick = async () => {
-        try {
-            const id = sessionUser?._id
-            const endpoint_URL = `http://localhost:4000/accounts/update-owner/${id}`
-            const payload = { name: newName };
-            
-            await axios.put(endpoint_URL, payload);
-            setIsEditing(false);
-        }catch(err) {
-            console.log(err)
-
-        }
-    }
     return (
         <div className="profile-header">
             <div className="profile-img-container">
                 <img src="" alt=""/>
-                {isEditing ? (
-                    <div>
-                        <input 
-                            type="text" 
-                            value={newName} 
-                            onChange={HandleNameChange}
-                        />
-                        <button onClick={handleSaveClick}> Save </button>
-                    </div>
-                ) : (
-                    <div>
-                         <h1>{newName}</h1>
-                         <button onClick={handleEditClick}> Edit </button>
-                    </div>
-                )}
+                <h1>{sessionUser?.name}</h1>
             </div>
         </div>
     )
 }
+
+// export const ProfileHeader = ({ sessionUser }) => {
+//     const [isEditing, setIsEditing] = useState(false);
+//     const [newName, setNewName] = useState(sessionUser?.name)
+
+//     const handleEditClick = () => {
+//         setIsEditing(true);
+//     }
+
+//     const HandleNameChange = (e) => {
+//         setNewName(e.target.value);
+//     }
+
+//     const handleSaveClick = async () => {
+//         try {
+//             const id = sessionUser?._id
+//             const endpoint_URL = `http://localhost:4000/accounts/update-owner/${id}`
+//             const payload = { name: newName };
+            
+//             await axios.put(endpoint_URL, payload);
+//             setIsEditing(false);
+//         }catch(err) {
+//             console.log(err)
+
+//         }
+//     }
+//     return (
+//         <div className="profile-header">
+//             <div className="profile-img-container">
+//                 <img src="" alt=""/>
+//                 {isEditing ? (
+//                     <div>
+//                         <input 
+//                             type="text" 
+//                             value={newName} 
+//                             onChange={HandleNameChange}
+//                         />
+//                         <button onClick={handleSaveClick}> Save </button>
+//                     </div>
+//                 ) : (
+//                     <div>
+//                          <h1>{newName}</h1>
+//                          <button onClick={handleEditClick}> Edit </button>
+//                     </div>
+//                 )}
+//             </div>
+//         </div>
+//     )
+// }
