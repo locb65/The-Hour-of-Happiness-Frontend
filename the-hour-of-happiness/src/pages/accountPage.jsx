3d6bb0677f3c74e84d4b255ad7bce9ce662640ff
locb65 <locb65@gmail.com>
2023-05-22 21:00:07 -0400

    imported account component to account page

--- the-hour-of-happiness/src/pages/accountPage.jsx
+++ the-hour-of-happiness/src/pages/accountPage.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import { AccountComponent } from "./components/accountComponent";
 
 export const AccountPage =() => {
-    
+    return (
+        <div className="account-page-container">
+            <AccountComponent/>
+        </div>
+    )
 }
