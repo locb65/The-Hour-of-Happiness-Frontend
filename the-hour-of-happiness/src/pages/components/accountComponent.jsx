5960979814a991af86454ede0240ea9633617237
locb65 <locb65@gmail.com>
2023-05-22 21:16:08 -0400

    email and password now displayed

--- the-hour-of-happiness/src/pages/components/accountComponent.jsx
+++ the-hour-of-happiness/src/pages/components/accountComponent.jsx
@@ -6,12 +6,14 @@ export const AccountComponent = ({sessionUser}) =>{
             <h1>Account Page</h1>
             <div className="account-update-section">
                 <h2>Email</h2>
+                <p>{sessionUser.email}</p>
                 <input className="email-input" type="text" placeholder="Email"></input>
                 <button className="edit-email-btn">Edit</button>
                 <button className="update-email-btn">Button</button>
             </div>
             <div className="account-update-section">
                 <h2>Password</h2>
+                <p>{sessionUser.password}</p>
                 <input className="password-input" type="password" placeholder="Enter your password" />
                 <input className="password-input" type="password" placeholder="Confirm your password" />
                 <button className="edit-password-btn">Edit Password</button>
