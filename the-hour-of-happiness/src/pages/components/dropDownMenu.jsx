faa8862b97b818ef0f54d2839cfbd1ebacb3c926
Loc Bui <buda@Locs-MBP.Starry-inc.net>
2023-05-24 17:50:15 -0400

    removed unused code

--- the-hour-of-happiness/src/pages/components/dropDownMenu.jsx
+++ the-hour-of-happiness/src/pages/components/dropDownMenu.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 import './dropDownMenu.css';
 import axios from 'axios'
 
--- the-hour-of-happiness/src/pages/home.jsx
+++ the-hour-of-happiness/src/pages/home.jsx
@@ -1,4 +1,4 @@
-import React, {useState}from "react";
+import React from "react";
 import { HappyHourList } from "./components/happyHourListView";
 import './home.css'
 
